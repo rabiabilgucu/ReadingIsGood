7370851a439b4ebdb3d15b808c8a0292a3f0efd3
rabiabilgucu <rabiabilgucu9778@gmail.com>
2022-10-28 21:25:04 +0300

    Initial commit

---
+++ src/test/java/com/getir/ReadingIsGood/ReadingIsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getir.ReadingIsGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingIsGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
